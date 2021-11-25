6d9142aa5b961d17144bfa516fd04918dfb2f01d
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-25 18:32:31 -0500

    creacion estados

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    );
+
+
+const App = () => {
+
+  const divStyle = {
+    display : 'none'
   }
+  const [check, setCheck] = useState(divStyle)
+  const [show, setShow] = useState(divStyle)
+
+  const handleClick = (e) => {
+    console.log(e)
+    // setShow()
+  }
+
+
+  return (
+    <div className="wrapper">
+      <label><input type="checkbox" checket onClick={handleClick} /> Mostrar información importante</label>
+      <p style={show} >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  )
+  
 }
 
 export default App;
