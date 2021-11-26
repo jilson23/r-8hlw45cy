1ede685b2bdeef17cc947d7e9fa23506469e7320
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-26 14:21:50 -0500

    add check state and if parragraf

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 
 const App = () => {
 
-  const divStyle = {
-    display : 'none'
-  }
-  const [check, setCheck] = useState(divStyle)
-  const [show, setShow] = useState(divStyle)
+  const [check, setCheck] = useState(false)
 
-  const handleClick = (e) => {
-    console.log(e)
-    // setShow()
+  const handleChange = () => {
+    setCheck(!check);
   }
+
+  const parrafo =  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 
   return (
     <div className="wrapper">
-      <label><input type="checkbox" checket onClick={handleClick} /> Mostrar información importante</label>
-      <p style={show} >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <label><input type="checkbox" checked={check} onChange={handleChange} /> Mostrar información importante</label>
+      {
+        check ? parrafo : undefined
+      }
+     
     </div>
   )
   
